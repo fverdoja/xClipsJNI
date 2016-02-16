16a17bcba8a94d94d2cca685acebf7408c600586
Francesco Verdoja <f.verdoja@gmail.com>
2016-02-16 17:54:38 +0100

    Codice aggiornato per funzionare con ClipsJNI v0.5

---
+++ XCLIPSJNI/src/xclipsjni/RouterImpl.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xclipsjni;
+
+import net.sf.clipsrules.jni.Router;
+
+/**
+ *
+ * @author fra
+ */
+public class RouterImpl implements Router {
+
+    private String theName;
+    private int thePriority;
+    private boolean ungotten;
+    private int lastChar;
+
+    public RouterImpl(String name) {
+        this(name, 10);
+    }
+
+    public RouterImpl(String name, int priority) {
+        super();
+        theName = name;
+        thePriority = priority;
+        ungotten = false;
+    }
+
+    @Override
+    public int getPriority() {
+        return thePriority;
+    }
+
+    @Override
+    public String getName() {
+        return theName;
+    }
+
+    @Override
+    public boolean query(String routerName) {
+        if (routerName.equals("stdout")
+                || routerName.equals("stdin")
+                || routerName.equals("wwarning")
+                || routerName.equals("werror")
+                || routerName.equals("wtrace")
+                || routerName.equals("wdialog")
+                || routerName.equals("wclips")
+                || routerName.equals("wdisplay")) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void print(String routerName, String printString) {
+        System.out.print(printString);
+    }
+
+    @Override
+    public int getchar(String routerName) {
+        int rv = -1;
+
+        if (ungotten) {
+            ungotten = false;
+            return lastChar;
+        }
+
+        try {
+            rv = System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+
+        return rv;
+    }
+
+    @Override
+    public int ungetchar(String routerName, int theChar) {
+        if (ungotten) {
+            return -1;
+        }
+
+        lastChar = theChar;
+        ungotten = true;
+
+        return theChar;
+    }
+
+    @Override
+    public boolean exit(int exitCode) {
+        return true;
+    }
+}
